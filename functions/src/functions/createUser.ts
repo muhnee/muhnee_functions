bac2dcf7e04d8950d4bf502500bc8371a8044ab8
Eric Jiang <ericjiang97@gmail.com>
2019-11-26 16:22:54 +1100

    initial functions, firestore rules and storage rules setup

---
+++ functions/src/functions/createUser.ts
@@ -0,0 +1,20 @@
+import * as functions from "firebase-functions";
+import admin from "firebase-admin";
+
+admin.initializeApp(functions.config().firebase);
+
+export const createUser = functions.auth.user().onCreate(async user => {
+  const db = admin.firestore();
+
+  const { uid, email, displayName, emailVerified, photoURL } = user;
+  await db
+    .collection("users")
+    .doc(user.uid)
+    .set({
+      uid,
+      email,
+      displayName,
+      emailVerified,
+      photoURL
+    });
+});
