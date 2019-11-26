8b5bb4b67baa2069b88a5d729ba7e27e1c538cb8
Eric Jiang <ericjiang97@gmail.com>
2019-11-26 16:29:37 +1100

    export function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from 'firebase-functions';
+import { createUser } from "./functions/createUser";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +6,5 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+export const createFirestoreUser = createUser;
