53fa2c0150a6b543483bbd1dea565ae89c0d5e4f
Eric Jiang <ericjiang97@gmail.com>
2019-12-13 21:03:50 +1100

    updated logic for initialising first month

--- functions/src/functions/createUser.ts
+++ functions/src/functions/createUser.ts
@@ -25,13 +25,7 @@ export const createUser = functions.auth.user().onCreate(async user => {
     .collection("users")
     .doc(user.uid)
     .collection("budget")
-    .doc(
-      `${moment()
-        .year()
-        .toString()}-${moment()
-        .month()
-        .toString()}`
-    )
+    .doc(`${moment().year()}-${moment().month() + 1}`)
     .set({ year: moment().year(), month: moment().month() });
 
   await db
