ff96b93717e94ba928d1165f82806251e1e83344
Eric Jiang <ericjiang97@gmail.com>
2020-02-03 14:44:03 +1100

    initial work on break down duplicate code

---
+++ functions/src/services/UserService.ts
@@ -0,0 +1,60 @@
+import admin from "firebase-admin";
+import moment from "moment";
+
+import { DateRange } from "../utils/generalUtils";
+
+import { TransactionTypes } from "../types/Transaction";
+
+/**
+ * get's a users Categories defined for the budget app
+ *
+ * @param db The firestore instance
+ * @param uid The user's uid (via context.auth.uid)
+ * @param type (the type of transaction income/expense)
+ */
+export const getUserCategories = (
+  db: admin.firestore.Firestore,
+  uid: string,
+  type: TransactionTypes
+): Promise<admin.firestore.QuerySnapshot> => {
+  return db.collection(`/users/${uid}/categories/${type}/types`).get();
+};
+
+/**
+ * get's a users Categories defined for the budget app
+ *
+ * @param db The firestore instance
+ * @param uid The user's uid (via context.auth.uid)
+ * @param type (the type of transaction income/expense)
+ */
+export const getUserTransactions = (
+  db: admin.firestore.Firestore,
+  uid: string,
+  type: TransactionTypes | null = null,
+  date: moment.Moment = moment(),
+  dateReange: DateRange
+): Promise<admin.firestore.QuerySnapshot> => {
+  const firestoreMonth = `${date.year()}=${date.month() + 1}`;
+  const { startDate, endDate } = dateReange;
+
+  let baseQuery = db
+    .collection(`/users/${uid}/budget/${firestoreMonth}/transactions`)
+    .where(
+      "timestamp",
+      ">",
+      admin.firestore.Timestamp.fromDate(startDate.toDate())
+    )
+    .where(
+      "timestamp",
+      "<",
+      admin.firestore.Timestamp.fromDate(endDate.toDate())
+    );
+
+  if (type === null) {
+    return baseQuery.orderBy("timestamp", "desc").get();
+  }
+  return baseQuery
+    .where("type", "==", type)
+    .orderBy("timestamp", "desc")
+    .get();
+};
