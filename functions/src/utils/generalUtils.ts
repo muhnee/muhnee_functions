7d4de294fd10d129a92167656be359049c130d28
Eric Jiang <ericjiang97@gmail.com>
2020-02-07 16:16:44 +1100

    date range update

--- functions/src/utils/generalUtils.ts
+++ functions/src/utils/generalUtils.ts
@@ -9,25 +9,21 @@ export const getDateRange = (
   currentDate: moment.Moment,
   summaryType: "week" | "month" | "year"
 ): DateRange => {
-  let startDate = moment().startOf("week");
-  let endDate = moment().endOf("week");
-
   switch (summaryType) {
     case "week":
-      startDate = currentDate.subtract(1, "week");
-      endDate = currentDate.endOf("day");
-      break;
+      return {
+        startDate: currentDate.subtract(1, "week").startOf("day"),
+        endDate: currentDate.endOf("day")
+      };
     case "month":
-      startDate = currentDate.subtract(1, "month");
-      endDate = currentDate.endOf("day");
-      break;
+      return {
+        startDate: currentDate.subtract(1, "month").startOf("day"),
+        endDate: currentDate.endOf("day")
+      };
     case "year":
-      startDate = currentDate.subtract(1, "year");
-      endDate = currentDate.endOf("day");
-      break;
+      return {
+        startDate: currentDate.subtract(1, "year").startOf("day"),
+        endDate: currentDate.endOf("day")
+      };
   }
-  return {
-    startDate,
-    endDate
-  };
 };
