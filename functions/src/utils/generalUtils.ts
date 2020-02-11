39a8145861b5447fcba2503e9b0f1800ef5fe4a8
Eric Jiang <ericjiang97@gmail.com>
2020-02-11 11:30:11 +1100

    fix tslint errors

--- functions/src/utils/generalUtils.ts
+++ functions/src/utils/generalUtils.ts
@@ -37,6 +37,6 @@ export const getDateRange = (
  */
 export const isIsoDate = (str: string): Boolean => {
   if (!/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(str)) return false;
-  var d = new Date(str);
+  const d = new Date(str);
   return d.toISOString() === str;
 };
