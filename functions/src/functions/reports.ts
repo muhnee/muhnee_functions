1bb67e706a3c77950ffef6eb1ff2394b1d6ad006
Eric Jiang <ericjiang97@gmail.com>
2020-02-19 14:54:30 +1100

    finally fking got reports working - thats a lot of promises and chaining to get it working

---
+++ functions/src/functions/reports.ts
@@ -0,0 +1,111 @@
+import * as functions from "firebase-functions";
+import { HttpsError } from "firebase-functions/lib/providers/https";
+import moment from "moment";
+
+import * as GeneralUtils from "../utils/generalUtils";
+import admin from "firebase-admin";
+import { Transaction } from "../types/Transaction";
+import * as UserService from "../services/UserService";
+import { CategoryMap, Category } from "../types";
+import { ReportResponse } from "../types/response";
+
+const db = admin.firestore();
+
+export const getAllTaxDeductibleItem = functions.https.onCall(
+  async (data, context) => {
+    const start = data.start;
+
+    if (!GeneralUtils.isIsoDate(start)) {
+      throw new HttpsError("invalid-argument", "year must be a number");
+    }
+
+    if (!context.auth) {
+      throw new HttpsError("unauthenticated", "User unauthenticated");
+    }
+
+    const user = context.auth.uid;
+
+    const expenseCategories: FirebaseFirestore.QuerySnapshot = await UserService.getUserCategories(
+      db,
+      context.auth.uid,
+      "expense"
+    );
+
+    const categoryMap: CategoryMap = { income: {}, expense: {} };
+
+    expenseCategories.docs.forEach(doc => {
+      const docData = doc.data();
+
+      categoryMap.expense[doc.id] = {
+        id: doc.id,
+        name: docData.name,
+        icon: docData.icon
+      };
+    });
+
+    const startMoment = moment(start);
+
+    const startTimestamp: admin.firestore.Timestamp = admin.firestore.Timestamp.fromDate(
+      startMoment.toDate()
+    );
+    const endTimestamp: admin.firestore.Timestamp = admin.firestore.Timestamp.fromDate(
+      startMoment.add(1, "year").toDate()
+    );
+
+    const transactions = await db
+      .collection(`/users/${user}/budget`)
+      .where("timestamp", ">=", startTimestamp)
+      .where("timestamp", "<", endTimestamp)
+      .get()
+      .then(monthSnapshots => monthSnapshots.docs.map(snap => snap.ref))
+      .then(months => {
+        return Promise.all(
+          months.map(monthSnap =>
+            monthSnap
+              .collection("transactions")
+              .where("type", "==", "expense")
+              .where("taxDeductible", "==", true)
+              .get()
+              .then(doc => doc.docs)
+          )
+        );
+      })
+      .then(res => Promise.all(res))
+      .then(docs => docs.flat())
+      .then(trans =>
+        trans.map(doc => {
+          const docData = doc.data();
+
+          const category: Category =
+            docData.type === "expense"
+              ? categoryMap.expense[docData.category]
+              : categoryMap.income[docData.category];
+
+          const firestoreTimestamp: admin.firestore.Timestamp =
+            docData.timestamp;
+
+          const transaction: Transaction = {
+            type: docData.type,
+            amount: docData.amount,
+            description: docData.description,
+            category: category,
+            categoryName: category.name,
+            taxDeductible: docData.taxDeductible,
+            timestamp: moment(firestoreTimestamp.toDate()).toISOString(),
+            id: doc.id,
+            recurringDays: docData.recurringDays
+          };
+          return transaction;
+        })
+      )
+      .then(transactions => {
+        return transactions;
+      });
+
+    const resp: ReportResponse<Transaction[]> = {
+      timestamp: moment().toISOString(),
+      data: transactions
+    };
+    return resp;
+  }
+);
---
+++ functions/src/types/response.d.ts
@@ -0,0 +1,4 @@
+export interface ReportResponse<T> {
+  timestamp: string;
+  data: T;
+}
